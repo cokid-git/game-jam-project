6372cc2024f201c294c51d1da5fdbc5b4dfd28b1
cokid-git <cokid11@yahoo.com>
2025-09-27 12:56:27 -0400

    Fixes UI Buttons/ Creates foundation for settings menu/ beginning adding sounds

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+
+
 var level = 1
 
 var enableDarkness = false
+
+var volumeMusic = 50
+var volumeSound = 50
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+
+func _ready() -> void:
+	pass
+
+
 func _on_start_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 	
