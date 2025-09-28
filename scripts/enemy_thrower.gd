37f687063d4fca258bbbde64ecfdfc047e011ca9
cokid-git <cokid11@yahoo.com>
2025-09-27 22:27:26 -0400

    Animation logic for enemy

--- scripts/enemy_thrower.gd
+++ scripts/enemy_thrower.gd
@@ -10,12 +10,21 @@ var elapsedTime = 0
 var deltaTracker = 0
 
 func _ready():
+	
 	sfxPlayer.volume_db = (0.6*Global.volumeSound) - 80
 	elapsedTime = HAMMER_TIME / 2
+	play("idle")
 
 func _process(delta):
+	if delta >= HAMMER_TIME * 1 / 3:
+		play("throw")
+	else: if delta >= HAMMER_TIME * 2/ 3:
+		play("idle")
+	else: if delta >= HAMMER_TIME:
+		play("prep throw")
 	elapsedTime += delta
 	if elapsedTime >= HAMMER_TIME:
+		play("throw")
 		throwHammer()
 		sfxPlayer.play()
 		elapsedTime = 0
