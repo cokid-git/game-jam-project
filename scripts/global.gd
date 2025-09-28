afa779848250cf7c68ffbf4829e22b03913d0c14
cokid-git <cokid11@yahoo.com>
2025-09-27 21:37:01 -0400

    Added code for whistle when play button is pressed (however it is toggled off; I'm not sure if I like it)

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var musicPlayer : AudioStreamPlayer
+@export var whistle : AudioStreamPlayer
 #@onready var musicPlayer = $Music
 
 var audioLocation: String #Title, Level Selection, Level, Enemy Level
@@ -13,6 +14,8 @@ var enableDarkness = false
 var volumeMusic = 50
 var volumeSound = 50
 
+var canPlayWhistle = false
+
 func _ready() -> void:
 	currentAudio = audioLocation
 	
@@ -20,6 +23,15 @@ func _process(delta: float) -> void:
 	if currentAudio != audioLocation:
 		currentAudio = audioLocation
 		updateMusic()
+	if audioLocation == "Title":
+		canPlayWhistle = false
+		#canPlayWhistle = true
+	playWhistle()
 
 func updateMusic():
 	musicPlayer["parameters/switch_to_clip"] = str(currentAudio)
+	
+func playWhistle():
+	if canPlayWhistle == true and audioLocation == "Level Selection":
+		whistle.play()
+		canPlayWhistle = false
